00c67abcc8dc1413965c7e5f8cf7d5f38ecef618
B <AmanKumar.B@elemica.com>
2025-07-08 17:12:58 +0530

    new files added

---
+++ product/src/com/elemica/supply/chain/assign01/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package com.elemica.supply.chain.assign01;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tableNumber=7;
+		for(int i=0; i<10; i++) {
+			System.out.println(tableNumber*(i+1));
+		}
+
+	}
+
+}
